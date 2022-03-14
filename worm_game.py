387c97aad2c515701d8ca55db4069fb48d87fbb1
THAMATIP CHITPONG <6410110663@email.psu.ac.th>
2022-03-14 23:37:59 +0700

    feat: Create game app

--- worm_game.py
+++ worm_game.py
@@ -1,2 +1,15 @@
-def game():
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.uix.widget import Widget
+
+class HungryWormGame(Widget):
     pass
+        
+
+class HungryWormApp(App):
+    def build(self):
+        game = HungryWormGame()
+        return game
+
+if __name__ == '__main__':
+    HungryWormApp().run()
