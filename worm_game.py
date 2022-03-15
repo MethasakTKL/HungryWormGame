854f66ec24f1e7b47433b9d86e0b957107104b4c
Chanawat Thuasuphap <64101110092@email.psu.ac.th>
2022-03-15 15:55:09 +0700

    feat(core): create snake

--- worm_game.py
+++ worm_game.py
@@ -1,9 +1,14 @@
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.uix.widget import Widget
+from kivy.graphics import Rectangle
 
 class HungryWormGame(Widget):
-    pass
+    def __init__(self):
+        super().__init__()
+
+        with self.canvas:
+            self.snake = Rectangle(pos=(10, 10), size=(100, 100))
         
 
 class HungryWormApp(App):
