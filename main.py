53d2fc927031f7fe3b295333b80b71e26ce3da60
Methasak Thipkonglas <6410110429@psu.ac.th>
2022-03-25 00:08:39 +0700

    feat(game): Set Direction_values  chore: adding Requirment autopep8

--- main.py
+++ main.py
@@ -1,29 +1,40 @@
 
+from telnetlib import DO
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.metrics import sp
+from kivy import properties as kp
 
-#set size of window
+# set size of window
 SPRIZE_SIZE = sp(20)
 COLS = int(Window.width / SPRIZE_SIZE)
 ROWS = int(Window.height / SPRIZE_SIZE)
 
-#set worm lenght and speed
+# set worm lenght and speed
 LENGTH = 3
 MOVESPEED = .1
 
-#set direction
+# set direction
 LEFT = 'left'
 RIGHT = 'right'
 UP = 'up'
 DOWN = 'down'
 
+direction_values = {
+    LEFT: [-1, 0],
+    RIGHT: [1, 0],
+    UP: [0, 1],
+    DOWN: [0, -1]
+}
+
 
 class HungryWormApp(App):
-    def build(self):
-        return self.form
+    head = kp.ListProperty([0, 0])
+    worm = kp.ListProperty()
+
+    food = kp.ListProperty([0, 0])
 
 
 if __name__ == '__main__':
