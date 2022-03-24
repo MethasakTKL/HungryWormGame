0054fae82d3d5c3753953d37112e68452fe9a1c7
Methasak Thipkonglas <6410110429@psu.ac.th>
2022-03-24 23:57:19 +0700

    feat(game): Set Worm len & speed & direction

--- main.py
+++ main.py
@@ -5,9 +5,20 @@ from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.metrics import sp
 
+#set size of window
 SPRIZE_SIZE = sp(20)
 COLS = int(Window.width / SPRIZE_SIZE)
 ROWS = int(Window.height / SPRIZE_SIZE)
+
+#set worm lenght and speed
+LENGTH = 3
+MOVESPEED = .1
+
+#set direction
+LEFT = 'left'
+RIGHT = 'right'
+UP = 'up'
+DOWN = 'down'
 
 
 class HungryWormApp(App):
