25f5df9ef3825b3106176a8df3e2015e5c363824
Methasak Thipkonglas <6410110429@psu.ac.th>
2022-03-30 20:39:56 +0700

    test(check_in_bound): Test case 04 position -1, -1

--- tests/test_check_in_bounds.py
+++ tests/test_check_in_bounds.py
@@ -11,20 +11,26 @@ class CheckInBoundTest(unittest.TestCase):
             return False
 
 
-    def test_in_bounds(self):
+    def test_in_bounds_10_10(self):
         position = [10, 10]
         result = self.check_in_bounds(position)
         
         self.assertFalse(result)
     
-    def test_not_in_bounds(self):
+    def test_not_in_bounds_5_5(self):
         position = [5, 5]
         result = self.check_in_bounds(position)
 
-        self.assertTrue(result)
+        self.assertTrue(result) 
     
-    def test_not_in_bounds_20_20(self):
+    def test_in_bounds_20_20(self):
         position = [20, 20]
+        result = self.check_in_bounds(position)
+
+        self.assertFalse(result)
+    
+    def test_in_bounds_m1_m1(self):
+        position = [-1, -1]
         result = self.check_in_bounds(position)
 
         self.assertFalse(result)
