bd5c6c6f4ef9cfc98f836cdea4550d7753319902
Iffahan Suksuwan <jackcopsluncher@gmail.com>
2022-03-30 20:18:16 +0700

    test(check_in_bound): Test case 01

---
+++ tests/test_check_in_bounds.py
@@ -0,0 +1,18 @@
+import unittest
+
+
+class CheckInBoundTest(unittest.TestCase):
+    def check_in_bounds(self, position):
+        COLS = 10
+        ROWS = 10
+        if all(0 <= position[x] < dim for x, dim in enumerate([COLS, ROWS])):
+            return True
+        else:
+            return False
+
+
+    def test_in_bounds(self):
+        position = [10, 10]
+        result = self.check_in_bounds(position)
+        
+        self.assertTrue(result)
